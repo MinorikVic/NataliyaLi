43313197e6107fb11b0b5ec9dcd94f395be2c8c4
MinorikVic <pobeda1105.94@gmail.com>
2021-01-24 20:33:35 +0300

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-
-gulp.task('hello', function(done){
-    console.log('Привет, мир!');
-    done();
-});
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
-    gulp.watch("./*.html").on('change', browserSync.reload);
-});
+    watch('./*.html').on('change', browserSync.reload);
+    watch('./sass/**/*.sass', serveSass).on('change', browserSync.reload);
+    watch('./sass/**/*.scss', serveSass).on('change', browserSync.reload);
+    watch('./js/*.js').on('change', browserSync.reload);
+};
+
+function mincss() {
+    src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('dist'));
+};
+function serveSass() {
+    return src("./sass/**/*.sass", "./sass/**/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+exports.serve = bs
